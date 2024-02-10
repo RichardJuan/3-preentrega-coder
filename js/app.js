d8b9bef19f98fe6b3197baf4cfa64eedfd789116
RichardJuan <juanignaciorichard972@gmail.com>
2024-02-10 17:29:17 -0300

    aplicacion de formulario y funciones de tal

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
-let Nombre = prompt("¿Cual es su nombre?");
-let Apellido = prompt("Genial!! ¿y su apellido?");
-let Edad = prompt("y cuenteme, cual es su edad?");
-if (Edad >= 16){
-    console.log("Esperamos que disfrutes de nuestro sitio");
-}else{
-    alert("lo siento, vuelve cuando tengas +16");
-}
+let Formulario = document.getElementById("Formulario")
+//Aca le damos funcion al formulario del inicio
+Formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let inputs = e.target.children; //aclaramos al input como objetivo y lo dividimos por posiciones.
+    let Nombre = inputs[0].value;
+    let Nacimiento = inputs[1].value;
+    
+    sessionStorage.setItem("Nombre", Nombre); //cargamos el Nombre al SessionStorage.
+
+    sessionStorage.setItem("fechaDeNacimiento", Nacimiento); //Lo mismo con la fecha de nacimiento.
+
+    //traemos la fecha de nacimiento del formulario
+    const fechaDeNacimiento = new Date (Nacimiento);
+
+    //declaramos la fecha de hoy
+    const hoy = Date.now();
+    const miliSegundosXAño = 31536000000;
+    const edad = Math.floor((hoy - fechaDeNacimiento) / miliSegundosXAño) //dividimos la dif entre la fecha actual y el nacimiento por la camtidad de milisegundos por año y redondeamos hacia el numero entero mas bajo 
+    
+    //condicion?respuesta:respuestalternativa
+    let respuesta = document.getElementById("respuesta");
+    
+    edad >= 16?respuesta.innerText = `Bienvenido ${Nombre}, ingresa a "Productos" para que veas lo que tenemos para ofrecerte`: respuesta.innerText = "Recomenamos que seas mayor de 16 años para ingresar a nuestra pagina. Buena suerte, nos vemos pronto";
+});
+
+
+let Nombre = sessionStorage.getItem("Nombre")
