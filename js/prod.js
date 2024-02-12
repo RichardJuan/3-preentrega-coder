d3957a0afa8b0c2537a161f814d8b21106884a61
RichardJuan <juanignaciorichard972@gmail.com>
2024-02-12 18:19:32 -0300

    interaccion sin prompt ni alerts

--- js/prod.js
+++ js/prod.js
@@ -1,3 +1,9 @@
+function finalizarProceso(){
+InteraccionP.addEventListener("click", () => {
+        localStorage.setItem("carrito", carrito);
+        location.reload();
+    })
+}
 //Definicion del catalogo y sus elementos
 class Articulo {
     constructor(Marca, Modelo, Precio, Talle) {
@@ -16,144 +22,78 @@ const J5 = new Articulo("Nike", "JORDAN 5", 260000, 41);
 
 const Catalogo = [NSJ, AF1, AM90, J1, J5];
 const carrito = []
-/*
-//Calculos de cuotas respectivas a cada elemento del catalogo en funciones
-function FuncionStefanJanoski(){
-    let metodoDePago = prompt("¿contado o cuotas?");
-    if (metodoDePago === "contado"){
-        alert(`El monto a abonar será de ${NSJ.Precio}`);
-    }else if (metodoDePago === "cuotas"){
-        for (let c = 2; c <= 6; c++){
-            if (c === 3){
-                continue;
-            }
-            if (c === 5){
-                continue;
-            }
-            let cuota = ((Catalogo[0].Precio / 2) + 20500 / c);
-            alert("las opciones son " + c + " de " + cuota);
-        }
-    }else{
-        alert("Elija una opcion de las anteriores");
-    }
-};
-function FuncionAirForce1(){
-    let metodoDePago = prompt("¿contado o cuotas?");
-    if (metodoDePago === "contado"){
-        alert(`El monto a abonar será de ${AF1.Precio}`);
-    }else if (metodoDePago === "cuotas"){
-        for (let c = 2; c <= 6; c++){
-            if (c === 3){
-                continue;
-            }
-            if (c === 5){
-                continue;
-            }
-            let cuota = ((Catalogo[1].Precio / 2) + 35000 / c);
-            alert("las opciones son " + c + " de " + cuota);
-        }
-    }else{
-        alert("Elija una opcion de las anteriores");
-    }
-};
-function FuncionAirMax90(){
-    let metodoDePago = prompt("¿contado o cuotas?");
-    if (metodoDePago === "contado"){
-        alert(`El monto a abonar será de ${AM90.Precio}`);
-    }else if (metodoDePago === "cuotas"){
-        for (let c = 2; c <= 6; c++){
-            if (c === 3){
-                continue;
-            }
-            if (c === 5){
-                continue;
-            }
-            let cuota = ((Catalogo[2].Precio / 2) + 40000 / c);
-            alert("las opciones son " + c + " de " + cuota);
-        }
-    }else{
-        alert("Elija una opcion de las anteriores");
-    }
-
-};
-function FuncionJordan1(){
-    let metodoDePago = prompt("¿contado o cuotas?");
-    if (metodoDePago === "contado"){
-        alert(`El monto a abonar será de ${J1.Precio}`);
-    }else if (metodoDePago === "cuotas"){
-        for (let c = 2; c <= 6; c++){
-            if (c === 3){
-                continue;
-            }
-            if (c === 5){
-                continue;
-            }
-            let cuota = ((Catalogo[3].Precio / 2) + 110000 / c);
-            alert("las opciones son " + c + " de " + cuota);
-        }
-    }else{
-        alert("Elija una opcion de las anteriores");
-    }
-};
-function FuncionJordan5(){
-    let metodoDePago = prompt("¿contado o cuotas?");
-    if (metodoDePago === "contado"){
-        alert(`El monto a abonar será de ${J5.Precio}`);
-    }else if (metodoDePago === "cuotas"){
-        for (let c = 2; c <= 6; c++){
-            if (c === 3){
-                continue;
-            }
-            if (c === 5){
-                continue;
-            }
-            let cuota = ((Catalogo[4].Precio / 2) + 130000 / c);
-            alert("las opciones son " + c + " de " + cuota);
-        }
-    }else{
-        alert("Elija una opcion de las anteriores");
-    }
-};
-*/
-
+//declaracion de Monto y filtrado de catalogo para opciones disponibles
 let cartera = document.getElementById("cartera");
 cartera.addEventListener("submit", (e) => {
     e.preventDefault();
     let inputs = e.target.children;
     let Monto = inputs[0].value;
     sessionStorage.setItem("Monto", Monto);
-    //Filtrado y definicion de articulos en base al monto disponible 
+//Filtrado y definicion de articulos en base al monto disponible 
     let Filtrado = Catalogo.filter ((item) => item.Precio <= Monto);
     const accesible = Filtrado.map((elemento) => elemento.Modelo);
+    sessionStorage.setItem("accesible", accesible);
     let opciones = document.getElementById("opciones");
-    opciones.innerText = `Con $${Monto} a dispocision, tenemos para ofrecerte: ${accesible}`
+//operador ternario en caso de que el monto sea menor al minimo
+    Monto<45000?opciones.innerText="Lo siento, no contamos con ninguna unidad de ese monto":opciones.innerText = `Con $${Monto} a dispocisión, tenemos para ofrecerte: ${accesible}`;
 });
 //traemos el formulario de los productos
 let InteraccionP = document.getElementById("InteraccionP");
+//Eleccion de articulo del catalogo
 InteraccionP.addEventListener("submit", (e) => {
     e.preventDefault();
     let inputs = e.target.children;
-    let eleccion = inputs[1].value;
-    sessionStorage.setItem("eleccion", eleccion);
-});
-
-let eleccion = Catalogo.find (Item => Item.Modelo === (sessionStorage.getItem("eleccion").toUpperCase));
-
-//calculo de cuotas, que reemplazara lo comentado al inicio.
-for (let c = 2; c <= 6; c++){
-    if (c === 3){
-        continue;
-    }
-    if (c === 5){
-        continue;
-    }
-    let cuota = ((eleccion.Precio / 2) * 0.15 / c);
+    let modeloSelected = (inputs[1].value).toUpperCase();
+    sessionStorage.setItem("modeloSelected", modeloSelected);
+    
+//Condicion para verificar la existencia del articulo
+    if (!Catalogo.some((Item) => Item.Modelo === modeloSelected)){
+        let accesible = sessionStorage.getItem("accesible");
+        opciones.innerText =`Deposite alguna de las opciones anteriores (${accesible})`;
+    }else{
+//Al encontrar el producto se dan las opciones de pago
+        let precioContado = (Catalogo.find(item => item.Modelo === modeloSelected).Precio)
+        opciones.innerText = `Para el pago de ${modeloSelected} tenemos las opciones de CONTADO o CUOTAS, ¿Que prefiere?`;
+        InteraccionP.addEventListener("submit", (e) => {
+            e.preventDefault;
+            let IM = document.getElementById("IM");
+            let metodoDePago = (IM.value).toUpperCase();
+//calculo de cuotas.
+            const cuotas = []
+            for (let c = 2; c <= 6; c++){
+                if (c === 3){
+                    continue;
+                }
+                if (c === 5){
+                    continue;
+                }
+                let calculoCuotas = ((precioContado / 2) + (precioContado * 0.21) / c);
+                respuestaCuotas = `${c} de $${calculoCuotas}`;
+                cuotas.push(respuestaCuotas);
+                };
+//eleccion entre CUOTAS o CONTADO.
+            if (metodoDePago === "CUOTAS"){
+                opciones.innerText = `Las opciones son ${cuotas}.
+                Presione el boton para añadir al carrito.`;
+                IM.remove();
+                finalizarProceso();
+            }else if (metodoDePago === "CONTADO"){
+                opciones.innerText = `El valor de la unidad es de $${precioContado}.
+                Presione el boton para añadir al carrito`;
+                IM.remove();
+                finalizarProceso();
+        }else{
+            opciones.innerText = "Seleccione algun metodo de pago";
+        };
+    });
 }
+});
+let modeloSelected = sessionStorage.getItem("modeloSelected");
 
-let Monto = sessionStorage.getItem("Monto");
 
 
-            /*let agregar = prompt(`¿Que articulo desea agregar? ${accesible}`)
+
+/*let agregar = prompt(`¿Que articulo desea agregar? ${accesible}`)
 if(accesible.includes(agregar)){
     carrito.push(agregar);
     let elementoSeleccionado = Catalogo.find((elemento) => elemento.Modelo === agregar)
@@ -170,4 +110,4 @@ let costoTotal = carrito.reduce((total, elemento) => total + Catalogo.find((arti
 
 if(cuponIngresado === `OG${cupon}`){
     alert(`El total a abonar sera de ${costoTotal}`)
-};*/
+*/
