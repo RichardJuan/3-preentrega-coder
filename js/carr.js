da63116cb5c3e921e04d650a5cb43adb1582b317
RichardJuan <juanignaciorichard972@gmail.com>
2024-03-20 16:45:20 -0300

    finalizacionDeJavascript

--- js/carr.js
+++ js/carr.js
@@ -3,37 +3,30 @@ const formCarrito = document.getElementById("formCarrito");
 const compra = document.getElementById("compra");
 const valor = document.getElementById("valor");
 
-let cupon = document.getElementById("cupon");
-const textCupon = document.getElementById("textCupon")
-const og = localStorage.getItem("OG");
-const ingresarCupon = document.getElementById("ingresarCupon");
-const enviarCupon = document.getElementById("formCarrito");
-
 const valorTotal = document.getElementById("valorTotal");
-const articuloCompra = JSON.parse(localStorage.getItem("compra"));
+const carrito = JSON.parse(localStorage.getItem("carrito"));
 
-const botonCompra = document.getElementById("botonC")
-const botonEnviar = document.getElementById("botonE");
+const botonCompra = document.getElementById("boton")
+if(carrito !== null){
+    compra.innerText = ""
+}
 
-let valorTotalIncompleto = articuloCompra.reduce((valorinc, item) => valorinc + item.Precio, 0);
-const descuento = (valorTotalIncompleto - (valorTotalIncompleto * 0.9))
-valorTotal.innerText = `$${(valorTotalIncompleto)}`
-compra.innerText = `${articuloCompra[0].Modelo}`;
-valor.innerText = `$${articuloCompra[0].Precio}`;
+const costo = carrito.reduce((
+    (accumulator, item) => accumulator + item.Precio), 0);
 
-enviarCupon.addEventListener("submit", (e) => {
-    const valorTotalFinal = valorTotal.innerText.split("$")[1] - descuento;
-    e.preventDefault();
-    let cuponIngresado = ingresarCupon.value
-    if(cuponIngresado === og){
+carrito.forEach((item) =>{
+    const div = document.createElement("div")
+    div.innerHTML = `
+    <div class="" style="width: 20rem;">
+    <h5 class="">${item.Modelo}</h5>
+    <p class="">$${item.Precio}</p>
+    </div>
+    `
+    compra.append(div);
+})
 
-        cupon.innerText = `-$${descuento}`;
-        valorTotal.innerText = valorTotalFinal;
-        ingresarCupon.remove();
-        textCupon.remove();
-        botonEnviar.remove()
-    }else{}
-});
+valorTotal.innerText= `$${costo}`
+
 botonCompra.addEventListener("click", () => {
     formCarrito.remove();
     agradecimiento.innerText = "GRACIAS POR SU COMPRA"
--- js/prod.js
+++ js/prod.js
@@ -5,34 +5,9 @@ function filtradoDeProd(productosFiltrado){
     for (let i = 0; i < productosFiltrado.length; i++) {
         const elementosCard = document.getElementById(productosFiltrado[i].id);
         elementosCard.remove();
-    
-}} 
-function finalizarProceso(){ //Definicion del proceso final al añadir al carrito
-InteraccionP.addEventListener("click", () => {
-    localStorage.setItem("compra", JSON.stringify(carrito));
-    console.log(carrito);
-    opciones.innerText = `Graciass, DE REGALO por su primera compra obtiene un cupon del 10%, guardelo "${cuponGenerado}"`
-    boton.remove();
-    const Toast = Swal.mixin({ //Sweet Alert para cartel superior derecho indica que se cargo bien
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-        toast.onmouseenter = Swal.stopTimer;
-        toast.onmouseleave = Swal.resumeTimer;
-        }
-    });
-    Toast.fire({
-        icon: "success",
-        title: "Añadido al Carrito con exito"
-    });
-    })
-}
-//Generacion de cupon de descuento
-let cuponGenerado = `OG${(Math.round(Math.random()* 100 + 200))}`;
-localStorage.setItem("OG", cuponGenerado);
+    }
+} 
+
 class Articulo { //Definicion del catalogo y sus elementos
     constructor(id, foto, Modelo, Precio) {
         this.id = id;
@@ -42,40 +17,6 @@ class Articulo { //Definicion del catalogo y sus elementos
     }
 }
 //traeyendo elementos
-const catalogo = []
-const trayendoElementos = async () => {
-    try{
-        const response = await fetch("../data.json");
-        const data = await response.json();
-        data.forEach((item) => {
-            const div = document.createElement("div");
-            div.setAttribute("id", item.id);
-            div.classList.add("prod")
-            div.innerHTML = `
-            <div class="card m-5 col-md-4" style="width: 20rem;">
-            <img class="card-img-top" src="${item.foto}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">${item.Modelo}</h5>
-                <p class="card-text">$${item.Precio}</p>
-                <a href="#" class="btn btn-primary">Agregar a carrito</a>
-                </div>
-            </div>
-            `
-            listadoDeProd.append(div);
-        const articulo = new Articulo (item.id, item.foto, item.Modelo, item.Precio);
-
-
-        catalogo.push(articulo)
-        });
-        
-
-    }
-    catch(error){
-        console.log(error);
-    }
-}
-trayendoElementos();
-
 const carrito = [] //array de carrito
 let cartera = document.getElementById("cartera");//declaracion de Monto y filtrado de catalogo para opciones disponibles
 cartera.addEventListener("click", () => {
@@ -98,55 +39,59 @@ cartera.addEventListener("click", () => {
     filtradoDeProd(Filtrado);
     });
 });
-let InteraccionP = document.getElementById("InteraccionP");//traemos el formulario de los productos
-InteraccionP.addEventListener("submit", (e) => {//Eleccion de articulo del catalogo
-    e.preventDefault();
-    let inputs = e.target.children;
-    let modeloSelected = (inputs[1].value).toUpperCase();
-    const modeloCompleto = Catalogo.filter((item) => item.Modelo === modeloSelected); 
-    localStorage.setItem("modeloSelected", JSON.stringify(modeloCompleto));
+const catalogo = []
+const trayendoElementos = async () => {
+    try{
+        const response = await fetch("../data.json");
+        const data = await response.json();
+        data.forEach((item) => {
+            const div = document.createElement("div");
+            div.setAttribute("id", item.id);
+            div.classList.add("prod")
+            div.innerHTML = `
+            <div class="card m-5 col-md-4" style="width: 20rem;">
+            <img class="card-img-top" src="${item.foto}" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">${item.Modelo}</h5>
+            <p class="card-text">$${item.Precio}</p>
+            <input type="submit" class="btn btn-primary" onClick="añadirCarrito(${item.id})" >
+            </div>
+            </div>
+            `
+            listadoDeProd.append(div);
+            const articulo = new Articulo (item.id, item.foto, item.Modelo, item.Precio);
+            
 
-    const cuotas = []//calculo de cuotas
-    for (let c = 2; c <= 6; c++){
-        if (c === 3){
-            continue;
-        }
-        if (c === 5){
-            continue;
-        }
-        let calculoCuotas = ((modeloCompleto[0].Precio / 2) + (modeloCompleto[0].Precio * 0.21) / c);
-        respuestaCuotas = `${c} de $${calculoCuotas}`;
-        cuotas.push(respuestaCuotas);
-    };
-    if (!Catalogo.some((Item) => Item.Modelo === modeloSelected)){//Condicion para verificar la existencia del articulo
-        let accesible = sessionStorage.getItem("accesible");
-        opciones.innerText =`Deposite alguna de las opciones anteriores (${accesible})`;
-    }else{
-        const inputOptions = new Promise((resolve) => {
-            setTimeout(() => {
-            resolve({
-                Contado: "Contado",
-                Cuotas: "Cuotas",
-            });
-            }, 1000);//Promesa con intervalo de 1 segundo (1000 mls)
+        catalogo.push(articulo)
         });
-        const { value: metodoDePago } = Swal.fire({//Sweet Alert con seleccion en radio para metodo de pago
-            title: "Seleccione metodo de pago",
-            input: "radio",
-            inputOptions,
-        }).then(metodoDePago => {//retencion de respuesta para definir entre las opciones cuotas o contado
-            if (metodoDePago.value === "Cuotas"){
-                opciones.innerText = `Las opciones son ${cuotas}.
-                Presione el boton para añadir al carrito.`;
-                IM.remove();
-                finalizarProceso();
-                carrito.push(modeloCompleto[0]);
-            }
-            if (metodoDePago.value === "Contado"){
-                opciones.innerText = `El valor de la unidad es de $${modeloCompleto[0].Precio}.
-                Presione el boton para añadir al carrito`;
-                IM.remove();
-                finalizarProceso();
-                carrito.push(modeloCompleto[0]);
-            }
-        })}})
+        
+
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+trayendoElementos();
+
+
+function añadirCarrito(id){
+    const articulo = catalogo.find((item) => item.id == id)
+    carrito.push(articulo)
+    console.log(articulo);
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+        const Toast = Swal.mixin({ //Sweet Alert para cartel superior derecho indica que se cargo bien
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: "success",
+        title: "Añadido al Carrito con exito"
+    });
+}
